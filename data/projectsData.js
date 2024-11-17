987ac98cbb057aece89f666adbc14c6e161d10b6
m.moza <m.moza@mplan.es>
2024-11-17 20:14:00 +0100

    init project schema

---
+++ data/projectsData.js
@@ -0,0 +1,20 @@
+const projectsData = [
+    {
+        'title': 'Portfolio Web',
+        'date': 'Noviembre 2024',
+        'short_description': 'Portafolio Web propio creado sin Frameworks',
+        'image_path': '',
+        'tecnologies': [
+            "HTML", "CSS", "JavaScript"
+        ]
+    },
+    {
+        'title': 'Portfolio Web',
+        'date': 'Noviembre 2024',
+        'short_description': 'Portafolio Web propio creado sin Frameworks',
+        'image_path': '',
+        'tecnologies': [
+            "HTML", "CSS", "JavaScript"
+        ]
+    }
+]
