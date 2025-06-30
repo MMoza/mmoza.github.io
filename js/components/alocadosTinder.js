1307ad6a4119838bb4c35960b7c946f08b687703
m.moza <m.moza@mplan.es>
2025-06-30 16:45:33 +0200

    init proyecto alocados

---
+++ js/components/alocadosTinder.js
@@ -0,0 +1,133 @@
+const DECISION_THRESHOLD = 75
+
+let isAnimating = false
+let pullDeltaX = 0 // --> distancia que la dard se est� arrastrando
+
+function startDrag (e) {
+    if (isAnimating) return
+
+    const actualCard = e.target.closest('article')
+
+    if(!actualCard) return
+
+    const startX = e.pageX ?? e.touches[0].pageX
+    
+    document.addEventListener('mousemove', onMove)
+    document.addEventListener('mouseup', onEnd)
+
+    document.addEventListener('touchmove', onMove, { passive: true })
+    document.addEventListener('touchend', onEnd, { passive: true })
+
+    function onMove(e) {
+        // Current postion of mouse or finger
+        const currentX = e.pageX ?? e.touches[0].pageX
+        
+        // Distance between the initial and current position
+        pullDeltaX = currentX - startX
+
+        // Si no hay distancia recorrida
+        if (pullDeltaX === 0) return
+
+        // Indicamos que estamos animando
+        isAnimating = true
+
+        // calculate de rotation of the card using the distance
+        const deg = pullDeltaX / 14
+
+        // apply transofrmation and styles to card
+        actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`
+        actualCard.style.cursor = 'grabbing'
+
+        // Change opacity of the choice info
+        const opacity = Math.abs(pullDeltaX) / 100
+        const isRight = pullDeltaX > 0
+
+        const choiceEl = isRight ? actualCard.querySelector('.choice.like') : actualCard.querySelector('.choice.nope')
+
+        choiceEl.style.opacity = opacity
+
+        console.log(opacity)
+    }
+    
+    function onEnd(e) {
+        // Remove the envent listeners
+        document.removeEventListener('mousemove', onMove)
+        document.removeEventListener('mouseup', onEnd)
+
+        document.removeEventListener('touchmove', onMove)
+        document.removeEventListener('touchend', onEnd)
+
+        // Saber si el usuario ha tomado una decisi�n
+        const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD
+
+        if (decisionMade) {
+            const goRight = pullDeltaX >= 0
+    
+            // add class according to the decision
+            actualCard.classList.add(goRight ? 'go-right' : 'go-left')
+            actualCard.addEventListener('transitionend', () => {
+                actualCard.remove()
+            }, { once: true })
+        } else {
+            actualCard.classList.add('reset')
+            actualCard.classList.remove('go-right', 'go-left')
+    
+            actualCard.querySelectorAll('.choice').forEach(choice => {
+              choice.style.opacity = 0
+            })
+        }
+
+        // reset the variables
+        actualCard.addEventListener('transitionend', () => {
+            actualCard.removeAttribute('style')
+            actualCard.classList.remove('reset')
+
+            pullDeltaX = 0
+            isAnimating = false
+        })
+
+        // reset the choice info opacity
+        actualCard
+            .querySelectorAll(".choice")
+            .forEach((el) => (el.style.opacity = 0));
+    }
+}
+
+async function loadCards() {
+    const cardsContainer = document.querySelector(".cards");
+
+    try {
+        const res = await fetch("../../data/projects/alocadosTinder/alocadsoData.json");
+        const profiles = await res.json();
+
+        const shuffled = profiles.sort(() => 0.5 - Math.random()).slice(0, 10);
+
+        const cardsHTML = shuffled.map(profile => {
+            return `
+          <article>
+            <img src="../../data/projects/alocadosTinder/${profile.image}" alt="${profile.name}, ${profile.age} a�os">
+            <h2>${profile.name} <span>${profile.age}</span></h2>
+            <div class="choice nope">NOPE</div>
+            <div class="choice like">LIKE</div>
+          </article>
+        `;
+        }).join("");
+
+        const endMessage = `
+            <span class="end-message">
+                No hay más Alocados cerca de ti... <br />
+                Vuelve a intentarlo más tarde
+            </span>
+        `;
+
+        cardsContainer.innerHTML = cardsHTML + endMessage;;
+
+    } catch (err) {
+        cardsContainer.innerHTML = "<p>Error cargando los perfiles.</p>";
+        console.error(err);
+    }
+}
+
+document.addEventListener('mousedown', startDrag)
+document.addEventListener('touchstart', startDrag, { passive: true })
+document.addEventListener("DOMContentLoaded", loadCards);
